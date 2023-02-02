9f317cdd1945c86dc8a605ff9a38d5be44f3a342
Ariel F. Garcia <arielg.incgarcia@gmail.com>
2023-02-02 10:30:00 +0000

    first commit

---
+++ storeItems/index.js
@@ -0,0 +1,127 @@
+class User {
+    constructor(username,goldCoins,silverCoins) {
+        this.username = username;
+        this.goldCoins = goldCoins;
+        this.silverCoins = silverCoins;
+        this.items = [];
+    }
+
+    getItems(){
+        return this.items;
+    }
+
+    setItem(item){
+        this.items.push(item);
+    }
+}
+
+let items = {
+    item: [
+        {
+            id: 1,
+            name: "Araña Gelatinosa",
+            description: "Consigue esta araña gelatinosa, excelente para decorar tus islas de Halloween!",
+            price: 2000,
+            coinType: "gold",
+            image: "arana.png",
+        },
+        {
+            id: 2,
+            name: "Egg Bohemio",
+            description: "Consigue este increible huevo artista de pascua!",
+            price: 5000,
+            coinType: "gold",
+            image: "bohemio_by_ariel.png",
+        },
+        {
+            id: 3,
+            name: "Coldman",
+            description: "Consigue este asombroso hombre de hielo!",
+            price: 2500,
+            coinType: "gold",
+            image: "coldman_by_ariel.png",
+        },
+        {
+            id: 4,
+            name: "Egg Nuwa",
+            description: "Consigue este huevo bañado en bronce y con una increible estrella!",
+            price: 8000,
+            coinType: "silver",
+            image: "nuwa_by_ariel.png",
+        },
+        {
+            id: 5,
+            name: "Octopus",
+            description: "Consigue este tierno y dulce pulpo, capaz de conmover a todo le que lo vea!",
+            price: 3000,
+            coinType: "gold",
+            image: "pulpo_high_quality_by_ariel.png",
+        },
+    ],
+}
+
+
+//c - Container ; cItems - container Items;
+const cItems = document.querySelector("#itemsMain");
+
+for(let i = 0; i<items.item.length; i++){
+    const cNewItem = document.createElement("div");
+    cNewItem.id = `itemCatalog-${items.item[i].id}`;
+    cNewItem.className = "item centerAll";
+
+    const newImg = new Image();
+    newImg.className = "imgItem";
+    newImg.src = `./img/${items.item[i].image}`;
+    newImg.alt = items.item[i].name;
+    newImg.title = items.item[i].name;
+
+    cNewItem.appendChild(newImg);
+    cItems.appendChild(cNewItem);
+
+    //console.log(items.item[i].name);
+}
+
+
+window.addEventListener("load",function(){
+    const username = prompt("Ingresa tu nombre de usuario","Ariel");
+
+    const user = new User(username,25000,15000);
+
+    const item = document.querySelectorAll(".item");
+
+    item.forEach(element => {
+        element.addEventListener("click",(e) => {
+
+            const itemActive = document.querySelectorAll(".itemNow");
+
+            itemActive.forEach(element => {
+                element.classList.remove("itemNow");
+            });
+
+
+            let id = 0;
+
+            if(e.target.className == "imgItem"){
+                
+                const parent = e.target.parentNode;
+                id = parent.id.split("-");
+                parent.className = parent.className + " " + "itemNow";
+
+            }else {
+                id = e.target.id.split("-");
+                e.target.className += " itemNow" ;
+            }
+
+            showItem(id[1]);
+        });
+    });
+
+});
+
+function showItem(itemId){
+    let imgItemSelected = document.querySelector("#itemSelected");
+    imgItemSelected.src = `./img/${items.item[itemId-1].image}`;
+
+    const lblPrice = document.querySelector("#lblPrice");
+    lblPrice.innerHTML = items.item[itemId-1].price;
+}

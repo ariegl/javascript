5884c039709da43b02ef742ff69fb78f439bb9b7
Ariel F <ariel.incgarcia@gmail.com>
2023-03-28 16:18:25 -0700

    JSPDF, useImperativeHandle, forwardRef

---
+++ react/testJsPdfApi/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState, useRef } from 'react'
+import Render from "react-dom/server"
+import './App.css'
+import {jsPDF} from "jspdf";
+import TemplateDocument from "./services/TemplateDocument"
+import html2canvas from "html2canvas"
+
+function App() {
+  const templatePDF = useRef();
+
+  function handleUpdateRef () {
+    templatePDF.current.generatePdf();
+  }
+
+  return (
+    <div className="App min-h-screen flex justify-center content-center flex-wrap bg-gray-800 text-white">
+      <div className='w-full flex justify-center m-10'>
+        <TemplateDocument ref={templatePDF}/>
+      </div>
+       <button className='bg-green-300 p-4 rounded rounded-xl text-gray-700 font-bold hover:text-yellow-200 hover:bg-gray-600' onClick={handleUpdateRef}>Generate PDF</button>
+    </div>
+  )
+}
+
+export default App
---
+++ react/testJsPdfApi/src/services/TemplateDocument.jsx
@@ -0,0 +1,50 @@
+import React, {useRef, useImperativeHandle } from 'react'
+import logo from "../assets/logoMunicipioCircle.jpg"
+import html2canvas from "html2canvas"
+import {jsPDF} from "jspdf";
+import Render from "react-dom/server"
+
+
+function TemplateDocument(props , ref) {
+
+    const myDiv = useRef(null)
+
+    function generatePdf () {
+        console.log("ENTRASE");
+        let data = myDiv.current;
+        let doc = new jsPDF("landscape", "pt", "a4");
+
+        html2canvas(data).then(function(canvas) {
+            doc.addImage(canvas.toDataURL(), 'JPEG',0,0);
+            doc.save("ARCHIVO XDDD")
+        });
+
+        console.log("DATA:",data);
+    }
+
+    useImperativeHandle(ref, () => ({
+        generatePdf
+    }));
+
+  return (
+    <div ref={myDiv} style={{width: '1130px', height: '877'}} className='text-black bg-white grid grid-cols-12 p-3 px-10 py-10'>
+        <div className='col-span-12' id="header">
+            <h1 className='text-5xl font-bold text-center m-1'>Preview Document</h1>
+        </div>
+        <div className='col-span-12 flex justify-center'>
+            <img className='my-10' src={logo} width="300px" alt="" />
+        </div>
+        <br/>
+        <div className='col-span-12'>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus, dolores. Repellendus officiis, doloribus facilis at dignissimos dolorum quod, aperiam nihil suscipit, cum placeat ducimus explicabo vero repudiandae accusantium architecto quibusdam!</p>
+        <br/>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, iste earum. Officia velit deserunt dolores molestiae ea natus voluptatem vitae atque illum facilis, cupiditate earum odit animi laboriosam doloribus? Dolorum.</p>
+        <br/>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit at, suscipit a, maiores esse minima nisi officiis harum ratione sint laboriosam, tempora adipisci dolorum in. Dignissimos veniam corporis maiores alias.</p>
+        </div>
+        <br/>
+    </div>  
+  )
+}
+
+export default React.forwardRef(TemplateDocument);

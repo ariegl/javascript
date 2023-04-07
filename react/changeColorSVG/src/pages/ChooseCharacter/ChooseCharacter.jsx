4286279d8dfec84d6571da1f27fddd13690c9e6a
Ariel <ariel.incgarcia@gmail.com>
2023-04-06 22:20:11 -0700

    features: mejoras en la carga del grid selector de personajes

--- react/changeColorSVG/src/pages/ChooseCharacter/ChooseCharacter.jsx
+++ react/changeColorSVG/src/pages/ChooseCharacter/ChooseCharacter.jsx
@@ -6,10 +6,10 @@ function ChooseCharacter() {
 
   return (
     <div id="ChooseCharacterView" className='h-screen w-full grid grid-cols-12'>
-      <div className='col-span-6 bg-gray-100 flex justify-center items-center flex-wrap'>
+      <div className='col-span-6 bg-gray-200 flex justify-center items-center flex-wrap'>
         <ViewCharacter/>
       </div>
-      <div className='col-span-6 bg-green-300'>
+      <div className='col-span-6 bg-gray-100'>
         <GridCharacter/>
       </div>
     </div>
--- react/changeColorSVG/src/pages/ChooseCharacter/GridCharacter/GridCharacter.jsx
+++ react/changeColorSVG/src/pages/ChooseCharacter/GridCharacter/GridCharacter.jsx
@@ -1,12 +1,18 @@
 import React, { useRef, useEffect, useState } from "react";
-import RastaFaceIcon from "../../../assets/pages/chooseCharacter/gridCharacter/rasta_face_icon.svg";
+import CONFIG from "@/utils/config.json";
 
 function GridCharacter() {
-  const [loadImage, setLoadImage] = useState(false);
+  //IMAGENES CARGADAS
   const [images, setImages] = useState([]);
-  const rastaFace = useRef(null);
-  const references = [];
 
+  //LISTA DE REFERENCIAS DE PERSONAJES !NECESARIAS PARA COLOREAR IMAGENES
+  const myRefObject = {
+    rastaFace: useRef(),
+    djFace: useRef(),
+    gataFace: useRef(),
+  }
+
+  //INFORMACION DE LOS PERSONAJES, COLORES , ETC.
   const characterData = {
     personaje: {
       dj: {
@@ -18,7 +24,7 @@ function GridCharacter() {
           audifonos: "audifonos",
         },
         colors: {
-          cabello: "#260f0f",
+          cabello: "gray",
           piel: "#f1d695",
           camisa: "#4a412c",
           pantalones: "#70706e",
@@ -39,94 +45,114 @@ function GridCharacter() {
           ojos: "#4a412c",
         },
       },
+      gata: {
+        render: {
+          cabello: "cabello",
+          piel: "piel",
+          orejas: "orejas",
+          cuello: "cuello",
+          botonGrande: "botonGrande",
+          botonChico: "botonChico"
+        },
+        colors: {
+          cabello: "red",
+          piel: "#ECDEC2",
+          orejas: "pink",
+          cuello: "pink",
+          botonGrande: "white",
+          botonChico: "red",
+        },
+      },
     },
   };
 
-  const handleChangeColor = () => {
-    console.log("change");
-
-    const personaje = rastaFace.current.contentDocument;
-
-    for (let key in characterData.personaje.rasta.render) {
-      const part = personaje.querySelectorAll(`.${key}`);
-
-      console.log(part);
-
-      part.forEach(function (element) {
-        element.style.fill = `${
-          characterData.personaje.rasta.colors[element.className.baseVal]
-        }`;
-      });
-    }
-  };
-
-  const handleLoadImage = () => {
-    setLoadImage(true);
-  };
-
   useEffect(() => {
-    //loadImage ? (console.log("CARGADO")/*, handleChangeColor()*/) : false;
-    async function loadImages() {
-      const svgs = import.meta.glob("../../../assets/pages/chooseCharacter/gridCharacter/*.svg");
+    //ESTA FUNCION CARGA LOS PERSONAJES DEL GRIDVIEW DONDE SE ELIGEN
+    async function loaderCharacters() {
+      async function loadImages() {
+        const svgs = import.meta.glob("../../../assets/pages/chooseCharacter/gridCharacter/*.svg");
+  
+        const loadedImages = await Promise.all(
+          Object.keys(svgs).map(async (path) => {
+            const svg = await svgs[path]();
+            const nameSplit = svgs[path].name.replace("../../../assets/pages/chooseCharacter/gridCharacter/","").split("_");
+            const name = nameSplit[0];
+            const content = svg.default;
+            return { path, content, name}
+          })
+        );
+  
+        setImages(loadedImages);
+      };
 
-      const loadedImages = await Promise.all(
-        Object.keys(svgs).map(async (path) => {
-          const svg = await svgs[path]();
-          const name = svgs[path].name.replace("../../../assets/pages/chooseCharacter/gridCharacter/","").split("_");
-          
-          const content = svg.default;
-          return { path, content}
-        })
-      );
+      await loadImages();
+      //console.log("TERMINARON DE CARGAR LOS PERSONAJES");
+    }
 
-      setImages(loadedImages);
-      console.log("LAS REF:",references);
-    };
-
-    loadImages();
+    loaderCharacters();
 
   }, []);
 
+  //ESTA FUNCION ESTA ENCARGADA DE PINTAR LAS IMAGENES UNA VEZ QUE YA HAYAN CARGADO
+  const handleLoadImage = (event) => {
+    console.log("CARGADO:",event.target.id);
+
+    let dataCharacter = event.target.id.split("_");
+    let characterLoaded = dataCharacter[0]+dataCharacter[1];
+    let characterName = dataCharacter[0];
+
+    async function paintElement (characterName,characterLoaded) {
+      for(let key in characterData.personaje[characterName].render){
+        const personaje = await myRefObject[characterName+"Face"].current.contentDocument;
+
+        const part =  await personaje.querySelectorAll(`.${key}`);
+
+        part.forEach(function (element) {
+          element.style.fill = `${
+            characterData.personaje[characterName].colors[element.className.baseVal]
+          }`;
+        });
+      }
+    }
+
+    paintElement(characterName,characterLoaded);
+  }
+
+  //CUANDO EL USUARIO SELECCIONE UN PERSONAJE
+  const handleSelectCharacter = (event) => {
+    console.log("CLICK:",event.target.id);
+  }
+
+  //UTILS
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
   return (
     <>
-      <div className="grid grid-cols-12 grid-rows-3 gap-6">
+      <div className="grid grid-cols-12 grid-rows-2">
         <h1 className="col-span-12 text-center font-bold text-4xl">
-          Elije tu personaje
+          Elige tu personaje
         </h1>
 
-      {images.map(({path, content}) => (
-        <div key={path} className="bg-red-300 col-span-4 flex justify-center items-center">
-          <object
-            key={"icon-"+path}
-            type="image/svg+xml"
-            data={content}
-            width={"200px"}
-            aria-label="Mi SVG"
-            //onLoad={handleLoadImage}
-            className="cursor-pointer"
-          />
+      {images.map(({path, content, name}) => (
+        <div key={path} className="col-span-4 flex justify-center flex-wrap">
+          <div id={"select_"+name} style={{width: '190px', height: '190px'}} className="cursor-pointer bg-gray-300 transition-transform transition-colors duration-300 rounded-full  flex justify-center items-center flex-wrap hover:bg-yellow-100 hover:border-solid hover:border-yellow-500 hover:border-4 hover:scale-105" onClick={handleSelectCharacter}>
+            <object
+              id={name+"_face"}
+              ref={myRefObject[name+"Face"]}
+              key={"icon-"+path}
+              type="image/svg+xml"
+              data={content}
+              width={"150px"}
+              aria-label="Mi SVG"
+              onLoad={handleLoadImage}
+              className="pointer-events-none"
+            />
+          </div>
+          <h1 className="w-full text-center font-bold pointer-events-none py-4 text-2xl">{capitalizeFirstLetter(name)}</h1>
         </div>
       ))}
-
-
-        <div className="bg-red-300 col-span-4 flex justify-center items-center">
-          <object
-            ref={rastaFace}
-            type="image/svg+xml"
-            data={RastaFaceIcon}
-            width={"200px"}
-            aria-label="Mi SVG"
-            //onLoad={handleLoadImage}
-            className="cursor-pointer"
-          />
-        </div>
-
-        <button
-          className="bg-blue-500 col-span-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={handleChangeColor}
-        >
-          Change color
-        </button>
       </div>
     </>
   );

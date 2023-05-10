5984671188658522812a2c2b7d2b172a21a4a867
Ariel F <ariel.incgarcia@gmail.com>
2023-05-10 16:58:18 -0700

    json web tokens & mongoose authentication

---
+++ next/project05/src/pages/api/auth/login.js
@@ -0,0 +1,38 @@
+import { sign } from "jsonwebtoken";
+import { serialize } from "cookie";
+import connect from "../../../../lib/mongodb";
+import User from "../../../../models/user";
+
+connect();
+
+export default async function loginHandler(req, res) {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({username,password}).lean(); //importante .lean
+
+  if (!user) return res.status(200).json({message: "error de credenciales"});
+
+  // expire in 30 days
+  const token = sign(
+    {
+      exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30,
+      username: username,
+      credits: user.credits
+    },
+    "secret"
+  );
+
+  const serialized = serialize("myTokenName", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 1000 * 60 * 60 * 24 * 30,
+    path: "/",
+  });
+
+  res.setHeader("Set-Cookie", serialized);
+  return res.status(200).json({
+    message: "Login successful",
+  });
+  
+}
---
+++ next/project05/src/pages/login.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+function Home() {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await axios.post("/api/auth/login", credentials);
+    console.log(res);
+
+    if (res.status === 200) {
+      router.push("/dashboard");
+    }
+  };
+
+  return (
+    <div className= "min-h-screen min-w-full flex justify-center items-center">
+      <div className="w-1/3 flex justify-center items-center">
+        <form className=" flex justify-center content-center flex-wrap" onSubmit={handleSubmit}>
+          <h1 className="w-full text-4xl text-center py-4">Login</h1>
+          <div className="w-2/3">
+            <input
+              type="text"
+              className="w-full py-2 px-4 mt-2 bg-gray-900 outline-1"
+              placeholder="user"
+              onChange={(e) =>
+                setCredentials({
+                  ...credentials,
+                  username: e.target.value,
+                })
+              }
+            />
+            <input
+              type="password"
+              placeholder="password"
+              className="w-full py-2 px-4 mt-2 bg-gray-900 outline-1"
+              onChange={(e) =>
+                setCredentials({
+                  ...credentials,
+                  password: e.target.value,
+                })
+              }
+            />
+          </div>
+          <div className="mt-4 w-full flex justify-center">
+            <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+              <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                  ENTRAR
+              </span>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Home;

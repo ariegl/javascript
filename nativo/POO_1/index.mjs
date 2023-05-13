419e45cb454b6bbc38e9820fd188a886d214e577
Ariel <ariel.incgarcia@gmail.com>
2023-05-12 23:04:40 -0700

    POO CLASSES

--- nativo/POO_1/index.mjs
+++ nativo/POO_1/index.mjs
@@ -1,5 +1,24 @@
 import User from "./userClass.mjs";
 
+const credentials = [
+    {
+        user: "admin",
+        password: "admin123",
+    },
+    {
+        user:"root",
+        password:"root123",
+    },
+    {
+        user:"user",
+        password:"user123",
+    },
+    {
+        user: "ariel",
+        password:"ariel123",
+    }
+]
+
 let users = [];
 const formLogin = document.getElementById("formLogin");
 const btnShowUsers = document.getElementById("showUsers");
@@ -7,11 +26,18 @@ const loggedData = document.getElementById("loggedData");
 
 function validateCredentials({username,password}) {
     return new Promise((resolve, reject) => {
-        if(username === "ariel" && password === "123123") {
-            users.push(new User("ariel","123456"));
-            resolve("DATOS CORRECTOS");
+
+        const founded = credentials.filter(element => {
+            if(element.user === username && element.password === password) {
+                return element;
+            }
+        })
+
+        if(founded.length > 0) {
+            users.push(new User(username,password));
+            resolve({status: 200, message: "Correcto"});
         } else {
-            reject("DATOS INCORRECTOS");
+            resolve({status: 400, message: "Incorrecto"});
         }
     })
 }
@@ -19,6 +45,7 @@ function validateCredentials({username,password}) {
 formLogin.addEventListener('submit', async (e) => {
     e.preventDefault();
 
+    //Obtenemos el formulario
     let formData = new FormData(formLogin);
 
     let data = {
@@ -26,13 +53,26 @@ formLogin.addEventListener('submit', async (e) => {
         password: formData.get("password")
     }
 
+    //verificamos las credenciales
     let res = await validateCredentials(data);
-    console.log("res:",res);
+    console.log(res);
 
-    let message = `${loggedData.value} 
-    Logged: ${data.username} is joinned `;
+    //Construimos un nuevo mensaje para el notificador
+    let pre = document.createElement("pre");
+    pre.setAttribute("data-prefix","$");
+    let code = document.createElement("code");
 
-    loggedData.value = message;
+    if(res.status === 200) {
+        code.classList.add("text-success")
+        code.textContent = `New user - ${data.username} is joinned`;
+    } else {
+        code.textContent = `Fail - ${data.username} invalid user credentials`;
+        code.classList.add("text-error");
+    }
+
+    pre.appendChild(code);
+    loggedData.appendChild(pre);
+
 });
 
 btnShowUsers.addEventListener("click", () => {

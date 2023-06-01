a4293a50fdd8b2ae08412fe335659005b262b90f
Ariel F <ariel.incgarcia@gmail.com>
2023-06-01 16:44:21 -0700

    ZUSTAND USE CONTEXT

--- next/project07/src/pages/index.js
+++ next/project07/src/pages/index.js
@@ -1,7 +1,8 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { useCounterStore } from '@/store/counterStore'
-import shallow from "zustand/shallow"
+import {shallow} from "zustand/shallow"
+import { useEffect } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,15 +11,27 @@ export default function Home() {
   const values = useCounterStore((state) => ({
     counter: state.count,
     title: state.title,
-  }),shallow)
+    posts: state.posts
+  }),shallow);
 
+  const {increment, getPost} = useCounterStore();
+
+  useEffect(() => {
+    getPost();
+  },[])
 
   return (
     <main
       className={`flex flex-wrap min-h-screen w-full content-center justify-center p-24 ${inter.className}`}
     >
       <h1 className='text-4xl w-full text-center'>Counter: {values.counter}</h1>
-      <h2>{values.title}</h2>
+      <h2 className='w-full text-center'>{values.title}</h2>
+      <button className='bg-slate-400 py-4 px-10 mt-4' onClick={() => {increment(10)}}>INCREMENT</button>
+
+      <hr/>
+      {
+        JSON.stringify(values.posts)
+      }
     </main>
   )
 }
--- next/project07/src/store/counterStore.js
+++ next/project07/src/store/counterStore.js
@@ -4,7 +4,17 @@ export const useCounterStore = create((set) =>
 ({
     count: 10,
     title: "some title",
+    posts: [],
     increment: (value) => set(state => ({
         count: state.count + value,
-    }))
+    })),
+    getPost: async () => {
+       const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+       const posts = await res.json();
+
+       set(state => ({
+        ...state,
+        posts
+       }))
+    }
 }))
